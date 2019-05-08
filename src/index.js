e7f5c95e1296afdceeaaa893e2361857f916905b
Taras Rushchak <taraskhust@gmail.com>
2019-05-08 17:15:54 +0300

    deployed final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import bootstrap from "bootstrap";
 import * as serviceWorker from './serviceWorker';
 
 import App from './components/app/app';
